dc7e0446e86f338a855eb598120a625e4bc47424
sandra <sandy_9011@live.com.mx>
2021-06-24 12:34:19 -0500

    version1

---
+++ src/app/components/items/items.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import{Item}from '../../modelos/item';
+import { ItemService } from '../../services/item.service';
+
+
+@Component({
+  selector: 'app-items',
+  templateUrl: './items.component.html',
+  styleUrls: ['./items.component.css']
+})
+export class ItemsComponent implements OnInit {
+
+  items: Item[] =[];
+  total: number = 0;
+
+  constructor(private itemService:ItemService) { }
+
+  ngOnInit(): void {
+    
+  //this.items=[];
+  this.items=this.itemService.getItems();
+  this.getTotal();
+  }
+  deleteItem(item:Item){
+    this.items = this.items.filter(x => x.id != item.id);
+    this.getTotal();
+  }
+
+  toggleItem(item:Item){
+    this.getTotal();
+
+  }
+  getTotal(){
+  this.total=this.items.filter(item=> !item.completed)
+                                     .map(x => x.quantity * x.price)
+                                     .reduce((acc, item)=> acc += item, 0);
+                                     console.log(this.total);
+  }
+}
